db612a9ec093e3329bad82178e1dce20b45b5b8f
Eberson Ramos <ebersonramos11@gmail.com>
2020-06-22 08:12:24 -0300

    [selenium] Removido atributos não usados.

--- src/test/java/com/selenium/marionette/common/SeleniumMarionetteBrowser.java
+++ src/test/java/com/selenium/marionette/common/SeleniumMarionetteBrowser.java
@@ -2,27 +2,21 @@ package com.selenium.marionette.common;
 
 public enum SeleniumMarionetteBrowser {
 
-    FIREFOX("firefox", "webdrivers/geckodriver.exe"),
-    CHROME("chrome", "webdrivers/chromedriver.exe");
+    FIREFOX("firefox"),
+    CHROME("chrome");
 
     private final String browserName;
-    private final String driverName;
 
-    private SeleniumMarionetteBrowser(String browserName, String driverName){
+    private SeleniumMarionetteBrowser(String browserName){
         this.browserName = browserName;
-        this.driverName = driverName;
     }
 
     public String getBrowserName() {
         return browserName;
     }
 
-    public String getDriverName() {
-        return driverName;
-    }
-
     @Override
     public String toString() {
-        return this.browserName.concat("_").concat(this.driverName);
+        return this.browserName;
     }
 }
